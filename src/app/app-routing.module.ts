3aac3e29be2981358cfd51f1a84785cc4723f357
Titus <you@example.com>
2019-02-07 19:28:56 +0300

    added bootstrap,fontawesome, menus,components, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SalesComponent }      from './pages/sales/sales.component';
+import { DashboardComponent }      from './pages/dashboard/dashboard.component';
+import { NotificationsComponent }      from './pages/notifications/notifications.component';
+import { CollectionsComponent }      from './pages/collections/collections.component';
+import { ReleasedComponent }      from './pages/released/released.component';
+import { PaymentsComponent }      from './pages/payments/payments.component';
+import { UsersComponent }      from './pages/users/users.component';
+import { DeliveriesComponent }      from './pages/deliveries/deliveries.component';
+import { MyaccountComponent }      from './pages/myaccount/myaccount.component';
+import { InvoicingComponent }      from './pages/invoicing/invoicing.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+{ path: 'sales', component: SalesComponent },
+{ path: 'dashboard', component: DashboardComponent },
+{ path: 'notifications', component: NotificationsComponent },
+{ path: 'collections', component: CollectionsComponent },
+{ path: 'released', component: ReleasedComponent },
+{ path: 'payments', component: PaymentsComponent },
+{ path: 'users', component: UsersComponent },
+{ path: 'deliveries', component: DeliveriesComponent },
+{ path: 'myaccount', component: MyaccountComponent },
+{ path: 'invoicing', component: InvoicingComponent }
+
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
