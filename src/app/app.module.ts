9342fcaa628b98a01d9d86e8d31b9d52d22180c7
Titus <you@example.com>
2019-02-13 11:26:01 +0300

    updated template with new components, services and sample upload form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {DataTableModule} from "angular-6-datatable";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { HttpClientModule } from '@angular/common/http';
+
+import { UploadService } from './services/fileuploader.service';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +22,10 @@ import { PaymentsComponent } from './pages/payments/payments.component';
 import { DeliveriesComponent } from './pages/deliveries/deliveries.component';
 import { UsersComponent } from './pages/users/users.component';
 import { InvoicingComponent } from './pages/invoicing/invoicing.component';
+import { HeaderComponent } from './theme/header/header.component';
+import { FooterComponent } from './theme/footer/footer.component';
+import { SidebarComponent } from './theme/sidebar/sidebar.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 
@@ -30,15 +41,21 @@ import { InvoicingComponent } from './pages/invoicing/invoicing.component';
     PaymentsComponent,
     DeliveriesComponent,
     UsersComponent,
-    InvoicingComponent
+    InvoicingComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
+    HttpClientModule,
     AngularFontAwesomeModule,
+    DataTableModule,FormsModule, ReactiveFormsModule,NgxDatatableModule, 
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UploadService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/deliveries/deliveries.component.ts
+++ src/app/pages/deliveries/deliveries.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+class Page {
+    //The number of elements in the page
+    size: number = 0;
+    //The total number of elements
+    totalElements: number = 0;
+    //The total number of pages
+    totalPages: number = 0;
+    //The current page number
+    pageNumber: number = 0;
+}
+class PagedData<T> {
+    data = new Array<T>();
+    page = new Page();
+}
 
 @Component({
   selector: 'app-deliveries',
   templateUrl: './deliveries.component.html',
   styleUrls: ['./deliveries.component.css']
 })
-export class DeliveriesComponent implements OnInit {
+export class DeliveriesComponent {
+ 
+    }
+  
 
-  constructor() { }
 
-  ngOnInit() {
-  }
 
-}
+
