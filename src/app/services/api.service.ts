1708b6a54af2772f2f7e7019b2961efeaa754db5
Titus <you@example.com>
2019-03-05 19:07:42 +0300

    form validation for partners

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,4 @@
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+
