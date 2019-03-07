70af8caf3cdfa4782589a8b165ffe8e16442e964
Titus <you@example.com>
2019-03-07 10:12:00 +0300

    Fix CORS issue using proxy

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,4 +1,37 @@
-import { Observable, of, throwError } from 'rxjs';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { catchError, tap, map } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from "rxjs/index";
+
+class ApiResponse {
+    status: number;
+    message: string;
+    result: any;
+    }
+  
+@Injectable()
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+  baseUrl: string = '/api/';
+
+  fetchData(resource): Observable<any> {
+    return this.http.get<any>(this.baseUrl+resource);
+  }
+
+  getRecordById(resource,id): Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(this.baseUrl + resource + id);
+  }
+
+  postData(resource,payload): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(this.baseUrl, payload);
+  }
+
+  updateRecord(resource,record): Observable<ApiResponse> {
+    return this.http.put<ApiResponse>(this.baseUrl + resource, record);
+  }
+  deleteRecord(resource,id): Observable<ApiResponse> {
+    return this.http.delete<ApiResponse>(this.baseUrl + resource + id);
+  }
+}
+
 
