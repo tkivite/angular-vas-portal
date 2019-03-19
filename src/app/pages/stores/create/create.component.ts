d79027371a81279cdbe7c25ed4adcdb0bf4bb7b8
Titus <you@example.com>
2019-03-19 18:38:21 +0300

    Store CRUD operations

--- src/app/pages/stores/create/create.component.ts
+++ src/app/pages/stores/create/create.component.ts
@@ -1,11 +1,11 @@
-import { SweetAlertService } from 'angular-sweetalert-service';
-import { BlockUI, NgBlockUI } from 'ng-block-ui';
-import { ToastrService } from 'ngx-toastr';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormGroup, FormControl, AbstractControl, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { CustomValidators } from 'ng2-validation';
-import { Router } from '@angular/router';
-
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { ToastrService } from 'ngx-toastr';
+//import { AlertService, AuthenticationService } from '../../../services';
+import { ApiService } from '../../../services/api.service';
 
 @Component({
   selector: 'app-create',
@@ -15,154 +15,118 @@ import { Router } from '@angular/router';
 export class CreateStoreComponent implements OnInit {
   @BlockUI() blockUI: NgBlockUI;
   public router: Router;
-  public formAdd: FormGroup;
-  public regionList: any;
+  public storeFormAdd: FormGroup;
   public activeInactive: any;
   public currentActive: any = 'DISABLED';
   public FormItem: FormArray;
   public errorMessage: any = 'SHOWERROR';
-  public masterLocations = [];
-  constructor(router: Router, fb: FormBuilder, public toastrService: ToastrService,
-    public alertService: SweetAlertService) {
-    this.router = router;
-    const products: FormArray = new FormArray([]);
-    const contacts: FormArray = new FormArray([]);
+  public PartnerOptions: any;
+
+  
+  constructor(router: Router, fb: FormBuilder, public toastrService: ToastrService,  public dataservice: ApiService) {
+    this.router = router;    
     const namePattern = /^[a-zA-Z ']{2,45}$/;
-    this.formAdd = fb.group({
-      entityName: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z0-9].*[\s\.]*$')])],
-      entityRegion: ['', Validators.compose([Validators.required])],
-      entityPaymentModel: ['', Validators.compose([Validators.required])],
-      entityMinDailyAmt: [0],
-      entityMaxDailyAmt: [0],
-      entityMinOrgAmt: [0],
-      entityMaxOrgAmt: [0],
-      entityMinTrxAmt: [0],
-      entityMaxTrxAmt: [0],
-      notificationShortName: [
-        '',
-        Validators.compose([
-          Validators.required,
-          Validators.minLength(4),
-          Validators.maxLength(7)
-        ])
-      ],
-      entityAuthMode: ['NATIVE'],
-      location: ['', Validators.compose([Validators.required])],
-      entityEmail: ['', Validators.compose([Validators.required, CustomValidators.email])],
-      entityCategory: ['', Validators.compose([Validators.required])],
-      products: products,
-      contacts: contacts,
-     });
-    // }, { validator: ''});
-    this.activeInactive = 'ENABLED'
-    
+    const kenyanMobileNoPattern = '^(254|0)(7([0-9]{8}))$';
+    const kenyanTillNoPattern = /^[0-9]{5,7}$/;
+
+    this.dataservice
+      .fetchData('partners').subscribe( data => {         
+        
+        if (data.status === 200) {
+           console.log(data.body);
+           this.PartnerOptions = data.body;
+           this.blockUI.stop();            
+        } else {
+        this.blockUI.stop();
+          this.toastrService.error(data.message);
+        }
+      }, err => {console.log("Bad things happened");  this.blockUI.stop();});
    
-    this.FormItem = <FormArray>this.formAdd.controls['contacts'];
-    this.FormItem.push(
-      new FormGroup({
-        name: new FormControl('', Validators.compose([Validators.required, Validators.pattern(namePattern)])),
-        type: new FormControl('', Validators.compose([Validators.required])),
-        value: new FormControl('', Validators.compose([Validators.required])),
-      })
-    );
+    this.storeFormAdd = fb.group({
+      
+      storePartner: ['', Validators.compose([Validators.required])],
+      storeName:     ['', Validators.compose([Validators.required, Validators.pattern(namePattern)])],
+      storeLocation: ['', Validators.compose([Validators.required])],
+      storeTarget: ['', Validators.compose([Validators.required])],
+      storeDisburseEmail:    ['', Validators.compose([Validators.required, CustomValidators.email])],
+      storeSourceid:    ['', Validators.compose([Validators.required, CustomValidators.email])],
+      storeManager:     ['', Validators.compose([Validators.required, Validators.pattern(namePattern)])],
+      storeManagerEmail:    ['', Validators.compose([Validators.required, CustomValidators.email])],
+      storeManagerMobile:   ['', Validators.compose([ Validators.required,Validators.pattern(kenyanMobileNoPattern)])],
+      storeContact:     ['', Validators.compose([Validators.required, Validators.pattern(namePattern)])],
+      storeContactEmail:    ['', Validators.compose([Validators.required, CustomValidators.email])],
+      storeContactMobile:   ['', Validators.compose([ Validators.required,Validators.pattern(kenyanMobileNoPattern)])],
+      
+      storeBank:     ['', Validators.compose([Validators.required, Validators.pattern(namePattern)])],
+      storeBankAccountName:    ['', Validators.compose([Validators.required, Validators.pattern(namePattern)])],
+      storeBankAccount:   ['', Validators.compose([ Validators.required])],
+      storeBankBranch:     ['', Validators.compose([Validators.required, Validators.pattern(namePattern)])],
+      storeMpesaTillNumber:    ['', Validators.compose([Validators.required, Validators.pattern(kenyanTillNoPattern)])],
+      storeMpesaPaybillNumber:   ['', Validators.compose([ Validators.required,Validators.pattern(kenyanTillNoPattern)])],
+            
+     });
+    
+    this.activeInactive = 'ENABLED'    
   }
 
   public ngOnInit() {
  
     this.blockUI.start('Processing');
-    this.currentActive = 'ENABLED';
-   
+    this.currentActive = 'ENABLED';   
       const searchParams = {
-        searchFields: [{ status: 1}]
-      };
+      searchFields: [{ status: 1}]      };
       const strParams = encodeURIComponent(JSON.stringify(searchParams));
-     
-    
-    this.formAdd.get('contacts')['controls'].forEach((contact, i) => {
-      this.subscribeChanges(contact);
-    });
-    this.blockUI.stop();
+      this.blockUI.stop();
+      
   }
-  subscribeChanges(contact: FormGroup) {
-     contact.get('type').valueChanges.subscribe(val => {
-      if (val === 'Email') {
-        contact.get('value').setValidators(
-          Validators.compose([Validators.required, CustomValidators.email])
-        );
-        contact.get('value').updateValueAndValidity();
-      } else {
-        contact.get('value').setValidators(
-          Validators.compose([
-            Validators.required,
-            Validators.pattern('^(254|0)(7([0-9]{8}))$')
-          ])
-        );
-        contact.get('value').updateValueAndValidity();
-      }
-    });
-  }
-
-  // Submitting Add Entity
+  get f() { return this.storeFormAdd.controls; }
+  // Submitting Store
   public onAddSubmit(form: FormGroup) {
+    
     if (form.valid) {
       this.errorMessage  = 'SHOWERROR';
-      this.blockUI.start('Adding Business Entity');
-      const perms = form.value.products;
-     
+      this.blockUI.start('Adding Store');  
       
       const postFormData = {
-        approvalStatus: 0,
-        minDailyAmt: form.value.entityMinDailyAmt,
-        maxDailyAmt: form.value.entityMaxDailyAmt,
-        minOrgAmt: form.value.entityMinOrgAmt,
-        maxOrgAmt: form.value.entityMaxOrgAmt,
-        minTrxAmt: form.value.entityMinTrxAmt,
-        maxTrxAmt: form.value.entityMaxTrxAmt,
-        paymentModel: form.value.entityPaymentModel,
-        authenticationMode: form.value.entityAuthMode,
-        notificationShortName: form.value.notificationShortName,
-        email: form.value.entityEmail,
-        businessCategoryId: form.value.entityCategory,
-        contacts: form.value.contacts,
+        name: form.value.storeName,
+        target: form.value.storeTarget,
+        location: form.value.storeLocation,
+        manager: form.value.storeManager,  
+        manager_email: form.value.storeManagerEmail, 
+        manager_phone: form.value.storeManagerMobile,  
+        contact_person: form.value.storeContact,   
+        contact_person_email: form.value.storeContactEmail,
+        contact_person_mobile: form.value.storeContactMobile,
+        disburse_email: form.value.storeDisburseEmail,
+        partner_id: form.value.storePartner,
         createdBy: 0,
-        regionId: form.value.entityRegion,
-        currentApprovalLevel: 0,
-        intrash: 'NO',
-        location: form.value.location,
-        maxApprovals: 0,
-        name: form.value.entityName,
-        status: 0,
-        timeCreated: new Date(),
-        timeUpdated: new Date(),
-        updatedBy: 0,
-        isAdminBe: 0,
-        throttleStatus: 0
-      };
-      
-      
+        source_id: form.value.storeSourceid,
+        bank: form.value.storeBank,   
+        bank_acct: form.value.storeBankAccountName,
+        bank_branch: form.value.storeBankBranch,
+        bank_acct_number: form.value.storeBankAccount,
+        mpesa_till_number: form.value.storeMpesaTillNumber,
+        mpesa_paybill_number: form.value.storeMpesaPaybillNumber      
+      };    
+
+
+      this.dataservice
+        .postData('stores', postFormData).subscribe( data => {         
+          
+          if (data.status === 201) {
+            this.toastrService.success(data.message);
+            this.router.navigate(['stores']);
+            this.blockUI.stop();
+          } else {
+            this.toastrService.error(data.message);
+            this.blockUI.stop();
+          }
+        }, err => {console.log("Problems!");  this.blockUI.stop();});
+       
     }
   }
   // On List
   onList() {
-    this.router.navigate(['pages/businessentity']);
-  }
-  
-  addContact() {
-    this.FormItem = <FormArray>this.formAdd.controls['contacts'];
-    this.FormItem.push(
-      new FormGroup({
-        name: new FormControl('', Validators.compose([Validators.required])),
-        type: new FormControl('', Validators.compose([Validators.required])),
-        value: new FormControl('', Validators.compose([Validators.required])),
-      })
-    );
-    this.formAdd.get('contacts')['controls'].forEach((contact, i) => {
-      this.subscribeChanges(contact);
-    });
-  }
-  removeContact(i) {
-    console.log(i);
-    this.FormItem = <FormArray>this.formAdd.controls['contacts'];
-    this.FormItem.removeAt(i);
+    this.router.navigate(['stores']);
   }
 }
