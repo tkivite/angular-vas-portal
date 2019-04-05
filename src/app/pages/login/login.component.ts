19c7e4aec7bc37b32b882af3d97001c6d12b1dac
tituskivite <tituskivite@lipalater.com>
2019-04-05 08:00:58 +0300

    onboarding fixes

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,76 +1,88 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
-import { ToastrService } from 'ngx-toastr';
-import { BlockUI, NgBlockUI } from 'ng-block-ui';
-import {AuthenticationService } from '../../services';
-import {ApiService } from '../../services/api.service';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { first } from "rxjs/operators";
+import { ToastrService } from "ngx-toastr";
+import { BlockUI, NgBlockUI } from "ng-block-ui";
+import { AuthenticationService } from "../../services";
+import { ApiService } from "../../services/api.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-    @BlockUI() blockUI: NgBlockUI;
-    loginForm: FormGroup;
-    loading = false;
-    submitted = false;
-    returnUrl: string;
-    constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private dataservice: ApiService,
-        public toastrService: ToastrService,
-        public alertService: ToastrService,
-        private authenticationService: AuthenticationService,
-        ) {
-        // redirect to home if already logged in
-        if (this.authenticationService.currentUserValue) {
-            this.router.navigate(['dashboard']);
-        }
+  @BlockUI() blockUI: NgBlockUI;
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private dataservice: ApiService,
+    public toastrService: ToastrService,
+    public alertService: ToastrService,
+    private authenticationService: AuthenticationService
+  ) {
+    // redirect to home if already logged in
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(["dashboard"]);
     }
+  }
 
-    ngOnInit() {
-        this.loginForm = this.formBuilder.group({
-            username: ['', Validators.compose([Validators.required])],
-            password: ['', Validators.compose([Validators.required])],
-        });
-    }
-    // convenience getter for easy access to form fields
-    get f() { return this.loginForm.controls; }
-    //submit login
-    public onSubmit(form: FormGroup) {
-      this.blockUI.start('Taking you home ....');
-      const postFormData = {
-            email: this.f.username.value,
-            password: this.f.password.value
-          };
-      this.authenticationService.login(postFormData)
-            .pipe(first())
-            .subscribe(
-                data => {
-                  console.log(data);
-                   // this.router.navigate([this.returnUrl]);
-                  this.router.navigate(['dashboard']);
-                  const firstname = data.user.firstname;
-                  this.blockUI.stop();
-                  this.toastrService.success('Login Successful', 'Welcome'+firstname, {
-                      timeOut: 10000
-                    });
-                },
-                error => {
-                  console.log(error);
-                  this.blockUI.stop();
-                  this.toastrService.error(error.error.error.user_authentication[0], 'Login Unsuccessful', {
-                      timeOut: 10000
-                    });
-                  });
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ["", Validators.compose([Validators.required])],
+      password: ["", Validators.compose([Validators.required])]
+    });
+  }
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.loginForm.controls;
+  }
+  //submit login
+  public onSubmit(form: FormGroup) {
+    this.blockUI.start("Taking you home ....");
+    const postFormData = {
+      email: this.f.username.value,
+      password: this.f.password.value
+    };
+    this.authenticationService
+      .login(postFormData)
+      .pipe(first())
+      .subscribe(
+        data => {
+          console.log(data);
+          // this.router.navigate([this.returnUrl]);
+          this.router.navigate(["dashboard"]);
+          const firstname = data.user.firstname;
+          this.blockUI.stop();
+          this.toastrService.success(
+            "Login Successful",
+            "Welcome" + firstname,
+            {
+              timeOut: 10000
+            }
+          );
+        },
+        error => {
+          console.log(error);
+          this.blockUI.stop();
+          this.toastrService.error(
+            error.error.error.user_authentication[0],
+            "Login Unsuccessful",
+            {
+              timeOut: 10000
+            }
+          );
         }
+      );
+  }
 
-    forgotPass() {
-          this.router.navigate(['forgotPassword']);
-        }
-      }
+  forgotPass() {
+    this.router.navigate(["forgotPassword"]);
+  }
+}
--- src/app/pages/partners/create/create.component.ts
+++ src/app/pages/partners/create/create.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormGroup, FormControl, AbstractControl, FormBuilder, Validators, FormArray } from '@angular/forms';
-import { CustomValidators } from 'ng2-validation';
-import { BlockUI, NgBlockUI } from 'ng-block-ui';
-import { ToastrService } from 'ngx-toastr';
-import { ApiService } from '../../../services/api.service';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Router } from "@angular/router";
+import {
+  FormGroup,
+  FormControl,
+  AbstractControl,
+  FormBuilder,
+  Validators,
+  FormArray
+} from "@angular/forms";
+import { CustomValidators } from "ng2-validation";
+import { BlockUI, NgBlockUI } from "ng-block-ui";
+import { ToastrService } from "ngx-toastr";
+import { ApiService } from "../../../services/api.service";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
+  selector: "app-create",
+  templateUrl: "./create.component.html",
   encapsulation: ViewEncapsulation.None
 })
 export class CreateBeComponent implements OnInit {
@@ -16,91 +23,125 @@ export class CreateBeComponent implements OnInit {
   public router: Router;
   public beFormAdd: FormGroup;
   public activeInactive: any;
-  public currentActive: any = 'DISABLED';
+  public currentActive: any = "DISABLED";
   public FormItem: FormArray;
-  public errorMessage: any = 'SHOWERROR';
+  public errorMessage: any = "SHOWERROR";
   public specialityOptions: any;
 
-
-  constructor(router: Router, fb: FormBuilder, public toastrService: ToastrService,  public dataservice: ApiService) {
+  constructor(
+    router: Router,
+    fb: FormBuilder,
+    public toastrService: ToastrService,
+    public dataservice: ApiService
+  ) {
     this.router = router;
     const namePattern = /^[a-zA-Z ']{2,45}$/;
-    const kenyanMobileNoPattern = '^(254|0)(7([0-9]{8}))$';
-   // const kenyanMobileNoPattern = '^(\+\d{1,3}[- ]?)?\d{10}$';
+    //const namep = /^[A-Z]([a-zA-Z0-9]|[- @\.#&!])*$/;
+    const kenyanMobileNoPattern = "^(254|0)(7([0-9]{8}))$";
+    const currentYear = new Date().getFullYear();
+    // const kenyanMobileNoPattern = '^(\+\d{1,3}[- ]?)?\d{10}$';
 
     this.specialityOptions = [
-      {id: '1', name: 'Electronics'},
-      {id: '2', name: 'Phones'},
-      {id: '3', name: 'Furniture'},
-      {id: '4', name: 'General'}];
+      { id: "1", name: "Electronics" },
+      { id: "2", name: "Phones" },
+      { id: "3", name: "Furniture" },
+      { id: "4", name: "General" }
+    ];
     // const specialityArray: FormArray = new FormArray([]);
 
     this.beFormAdd = fb.group({
-      orgName:     ['', Validators.compose([Validators.required, Validators.pattern(namePattern)])],
-      orgLocation: ['', Validators.compose([Validators.required])],
-      orgEmail:    ['', Validators.compose([Validators.required, CustomValidators.email])],
-      orgTelephone: ['', Validators.compose([Validators.required])],
-      orgMobile:   ['', Validators.compose([ Validators.required, Validators.pattern(kenyanMobileNoPattern)])],
-      orgYearsOfOperation: ['', Validators.compose([Validators.required, CustomValidators.number])],
-      orgSpeciality: ['', Validators.required]
-     });
-    this.activeInactive = 'ENABLED'
+      orgName: [
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(namePattern)
+        ])
+      ],
+      orgLocation: ["", Validators.compose([Validators.required])],
+      orgEmail: [
+        "",
+        Validators.compose([Validators.required, CustomValidators.email])
+      ],
+      orgTelephone: ["", Validators.compose([Validators.required])],
+      orgMobile: [
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(kenyanMobileNoPattern)
+        ])
+      ],
+      orgYearsOfOperation: [
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.min(currentYear - 200),
+          Validators.max(currentYear),
+          CustomValidators.number
+        ])
+      ],
+      orgSpeciality: ["", Validators.required]
+    });
+    this.activeInactive = "ENABLED";
   }
 
   public ngOnInit() {
-
-    this.blockUI.start('Processing');
-    this.currentActive = 'ENABLED';
+    this.blockUI.start("Processing");
+    this.currentActive = "ENABLED";
     const searchParams = {
-        searchFields: [{ status: 1}]
-      };
+      searchFields: [{ status: 1 }]
+    };
     const strParams = encodeURIComponent(JSON.stringify(searchParams));
     this.blockUI.stop();
   }
-  get f() { return this.beFormAdd.controls; }
+  get f() {
+    return this.beFormAdd.controls;
+  }
   // Submitting Add Entity
   public onAddSubmit(form: FormGroup) {
-
     if (form.valid) {
-      this.errorMessage  = 'SHOWERROR';
-      this.blockUI.start('Adding Partner');
+      this.errorMessage = "SHOWERROR";
+      this.blockUI.start("Adding Partner");
 
       const postFormData = {
         email: form.value.orgEmail,
         location: form.value.orgLocation,
         telephone: form.value.orgTelephone,
-        mobile: '+' + form.value.orgMobile,
+        mobile: "+" + form.value.orgMobile,
         year_of_incorporation: form.value.orgYearsOfOperation,
         name: form.value.orgName,
         speciality: form.value.orgSpeciality
       };
-      this.dataservice
-        .postData('partners', postFormData).subscribe( data => {
-
-
+      this.dataservice.postData("partners", postFormData).subscribe(
+        data => {
           if (data.status === 201) {
             this.toastrService.success(data.message);
-            this.router.navigate(['partners']);
+            this.router.navigate(["partners"]);
             this.blockUI.stop();
-            this.toastrService.success('Record Creation was successful');
+            this.toastrService.success("Record Creation was successful");
           } else {
             // this.toastrService.error(data.message);
             this.blockUI.stop();
-           // this.toastrService.error(data.message);
-            this.toastrService.success('There was a problem creating the record');
+            // this.toastrService.error(data.message);
+            this.toastrService.success(
+              "There was a problem creating the record"
+            );
           }
-        }, err =>
-        {
-          console.log('Something Went Wrong, We could not complete the request');
+        },
+        err => {
+          console.log(
+            "Something Went Wrong, We could not complete the request"
+          );
           console.log(err);
           this.blockUI.stop();
-          this.toastrService.error('Something Went Wrong, We could not complete the request');
-        });
-
+          this.toastrService.error(
+            "Something Went Wrong, We could not complete the request"
+          );
+        }
+      );
     }
   }
   // On List
   onList() {
-    this.router.navigate(['partners']);
+    this.router.navigate(["partners"]);
   }
 }
