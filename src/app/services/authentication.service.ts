3cce572d3c631fb587a434d58f414053b4ed2a27
tituskivite <tituskivite@lipalater.com>
2019-04-05 09:23:30 +0300

    api endpint for prod

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,43 +1,45 @@
-﻿import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+﻿import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
-
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class AuthenticationService {
-    private currentUserSubject: BehaviorSubject<any>;
-    public currentUser: Observable<any>;
-    baseUrl: string = '/api/';
-   // baseUrl = 'https://partner-portal-backend.herokuapp.com/';
+  private currentUserSubject: BehaviorSubject<any>;
+  public currentUser: Observable<any>;
+  // baseUrl: string = '/api/';
+  baseUrl = "https://partner-portal-backend.herokuapp.com/";
 
-    constructor(private http: HttpClient) {
-        this.currentUserSubject = new BehaviorSubject<any>(JSON.parse(localStorage.getItem('currentUser')));
-        this.currentUser = this.currentUserSubject.asObservable();
-    }
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<any>(
+      JSON.parse(localStorage.getItem("currentUser"))
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 
-    public get currentUserValue(): any {
-        return this.currentUserSubject.value;
-    }
+  public get currentUserValue(): any {
+    return this.currentUserSubject.value;
+  }
 
-    login(postData) {
-        return this.http.post<any>(this.baseUrl + 'authenticate', postData)
-            .pipe(map(user => {
-                console.log('from api' + user);
-                // login successful if there's a jwt token in the response
-                if (user && user.auth_token) {
-                    // store user details and jwt token in local storage to keep user logged in between page refreshes
-                    localStorage.setItem('currentUser', JSON.stringify(user));
-                    this.currentUserSubject.next(user);
-                }
+  login(postData) {
+    return this.http.post<any>(this.baseUrl + "authenticate", postData).pipe(
+      map(user => {
+        console.log("from api" + user);
+        // login successful if there's a jwt token in the response
+        if (user && user.auth_token) {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem("currentUser", JSON.stringify(user));
+          this.currentUserSubject.next(user);
+        }
 
-                return user;
-            }));
-    }
+        return user;
+      })
+    );
+  }
 
-    logout() {
-        // remove user from local storage to log user out
-        localStorage.removeItem('currentUser');
-        this.currentUserSubject.next(null);
-    }
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem("currentUser");
+    this.currentUserSubject.next(null);
+  }
 }
