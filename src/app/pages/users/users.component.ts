acbe040488935de32f8161bf8e802cf9c568328d
tituskivite <tituskivite@lipalater.com>
2019-04-24 11:08:55 +0300

    user separation

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -66,7 +66,37 @@ export class UsersComponent implements OnInit {
       data => {
         if (data.status === 200) {
           console.log(data.body);
-          this.data = data.body;
+          // this.data = data.body;
+          this.data = [
+            {
+              color: "red",
+              value: "#f00"
+            },
+            {
+              color: "green",
+              value: "#0f0"
+            },
+            {
+              color: "blue",
+              value: "#00f"
+            },
+            {
+              color: "cyan",
+              value: "#0ff"
+            },
+            {
+              color: "magenta",
+              value: "#f0f"
+            },
+            {
+              color: "yellow",
+              value: "#ff0"
+            },
+            {
+              color: "black",
+              value: "#000"
+            }
+          ];
           this.blockUI.stop();
         } else {
           this.blockUI.stop();
