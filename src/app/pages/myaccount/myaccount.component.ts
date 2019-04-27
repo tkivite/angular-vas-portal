0699829817e2fe3f781c59a0c450b868a14c321d
tituskivite <tituskivite@lipalater.com>
2019-04-27 15:06:06 +0300

    my account

--- src/app/pages/myaccount/myaccount.component.ts
+++ src/app/pages/myaccount/myaccount.component.ts
@@ -1,68 +1,24 @@
-import { Component, OnInit, ViewEncapsulation, ElementRef } from '@angular/core';
-import { FormGroup, FormControl, AbstractControl, FormBuilder, Validators, FormArray } from '@angular/forms';
-import {HttpClient, HttpParams, HttpRequest, HttpEvent,HttpResponse,HttpEventType} from '@angular/common/http';
-
-
-import { UploadService } from './../../services/fileuploader.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthenticationService } from "../../services/authentication.service";
 
 @Component({
-  selector: 'app-myaccount',
-  templateUrl: './myaccount.component.html',
-  styleUrls: ['./myaccount.component.css']
+  selector: "app-myaccount",
+  templateUrl: "./myaccount.component.html",
+  styleUrls: ["./myaccount.component.css"]
 })
 export class MyaccountComponent implements OnInit {
-public formAdd: FormGroup;
+  useraccount: any;
+  store: any;
 
-  constructor(public upload: UploadService,fb: FormBuilder,private elem: ElementRef) { 
-this.formAdd = fb.group({
-      datafile: ['', Validators.compose([Validators.required])],
-      fileName: ['', Validators.compose([Validators.required])]
-    });
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(
+      x => ((this.useraccount = x.user), (this.store = x.store))
+    );
   }
 
-  ngOnInit() {
-  }
-
-    onDropFile(event: DragEvent) {
-    event.preventDefault();
-   // this.uploadFile(event.dataTransfer.files);
-  }
-
-  // At the drag drop area
-  // (dragover)="onDragOverFile($event)"
-  onDragOverFile(event) {
-    event.stopPropagation();
-    event.preventDefault();
-  }
-
-  // At the file input element
-  // (change)="selectFile($event)"
- // selectFile(event) {
- //  this.uploadFile(event.target.files);
- // }
-public onUserSubmit(form: FormGroup) {
-    console.log('Adding File Upload');
-    const files = this.elem.nativeElement.querySelector('#selectFile').files;
-
-
-    let file: File = files[0];
-
-    this.upload.uploadFile("backend url/api/flash/upload", file)
-      .subscribe(
-        event => {
-          if (event.type == HttpEventType.UploadProgress) {
-            const percentDone = Math.round(100 * event.loaded / event.total);
-            console.log(`File is ${percentDone}% loaded.`);
-          } else if (event instanceof HttpResponse) {
-            console.log('File is completely loaded!');
-          }
-        },
-        (err) => {
-          console.log("Upload Error:", err);
-        }, () => {
-          console.log("Upload done");
-        }
-      )
-  }
-
+  ngOnInit() {}
 }
