4d7f667e112d9f744999c47f6b458444dadb38fc
tituskivite <tituskivite@lipalater.com>
2019-05-15 17:15:44 +0300

    added the component for listing and initiating invoices

--- src/app/pages/invoicing/invoicing.component.ts
+++ src/app/pages/invoicing/invoicing.component.ts
@@ -1,15 +1,99 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
+import { Router } from "@angular/router";
+import { BlockUI, NgBlockUI } from "ng-block-ui";
+import { ApiService } from "../../services/api.service";
 
 @Component({
-  selector: 'app-invoicing',
-  templateUrl: './invoicing.component.html',
-  styleUrls: ['./invoicing.component.css']
+  selector: "app-invoicing",
+  templateUrl: "./invoicing.component.html",
+  styleUrls: ["./invoicing.component.css"]
 })
-export class InvoicingComponent implements OnInit {
+export class InvoicingComponent {
+  @BlockUI() blockUI: NgBlockUI;
+  public router: Router;
+  public data = [];
+  searchKey = "";
+  current_page = 1;
+  total_records = 0;
+  page_size = 25;
+  total_pages = 1;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    router: Router,
+    public toastrService: ToastrService,
+    private dataservice: ApiService
+  ) {
+    this.router = router;
+    this.getData();
   }
+  // Load Grid Data
+  getData(searchKey = "", currentPage = 1) {
+    this.blockUI.start("Loading  invoices.....");
+    this.dataservice.fetchData("invoices", searchKey, currentPage).subscribe(
+      data => {
+        console.log(data);
+        if (data.status === 200) {
+          console.log(data.body);
+          this.data = data.body.invoices;
+          this.total_records = data.body.total_records;
+          this.page_size = this.data.length;
+          this.total_pages = Math.ceil(this.total_records / this.page_size);
+          this.data = data.body.invoices;
 
+          this.blockUI.stop();
+        } else {
+          this.blockUI.stop();
+          this.toastrService.error(data.message);
+        }
+      },
+      err => {
+        console.log("Something Went Wrong, We could not complete the request");
+        this.blockUI.stop();
+        this.toastrService.error(
+          "Something Went Wrong, We could not complete the request"
+        );
+      }
+    );
+  }
+  onView(data) {
+    console.log(data);
+    this.dataservice.EditFormData = data;
+    this.router.navigate(["invoices/view"]);
+  }
+  onCreate() {
+    this.dataservice.createInvoice().subscribe(
+      data => {
+        console.log(data);
+        if (data.status === 201) {
+          console.log(data.body);
+          this.dataservice.newInvoice = data.body;
+          this.router.navigate(["invoices/create"]);
+          this.blockUI.stop();
+        } else {
+          this.blockUI.stop();
+          this.toastrService.error(data.message);
+        }
+      },
+      err => {
+        // console.log("Something Went Wrong, We could not complete the request");
+        this.blockUI.stop();
+        this.toastrService.error(
+          "Something Went Wrong, We could not complete the request"
+        );
+      }
+    );
+  }
+  onSearch() {
+    console.log(this.searchKey);
+    this.getData(this.searchKey, 1);
+  }
+  loadPage(i) {
+    this.current_page = i;
+    this.getData(this.searchKey, i);
+  }
+  toggleRecordDetails(item) {
+    if (item.showDetails) item.showDetails = false;
+    else item.showDetails = true;
+  }
 }
